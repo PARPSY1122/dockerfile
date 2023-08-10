d31e82744bc5e477caf37efac4a1cd7319d7b6d2
PARPSY1122 <119403673+PARPSY1122@users.noreply.github.com>
2023-08-10 17:33:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "welcome to Marolix Technologies!"
+    return "Ghattamaneni Mahesh Babu is an Indian actor, producer, media personality, and philanthropist who works mainly in Telugu cinema. Wikipedia
+Born: 9 August 1975 (age 48 years), Chennai
+Upcoming movie: Guntur Kaaram
+Spouse: Namrata Shirodkar (m. 2005)
+Height: 1.83 m!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
